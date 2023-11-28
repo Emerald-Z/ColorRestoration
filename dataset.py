bfbdee1cb1c4c79de4d06bf695989c3077ec2d36
AddieFoote <addiefoote8@gmail.com>
2023-11-28 10:03:36 -0600

    datset

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import torch
 import os
+import random
 
 class ImgDataset(torch.utils.data.Dataset):
   def __init__(self, gray, color, rgb):
@@ -23,8 +24,16 @@ class ImgDataset(torch.utils.data.Dataset):
   def __len__(self):
     return len(self.paths)
 
-def generate_dataset(input_path, max_num, img_size):
-    for filename, i in zip(os.listdir(input_path), range(max_num)):
+def generate_dataset(input_path, max_num, img_size, path_base="", depth=1):
+    paths = os.listdir(input_path)
+    real_paths = []
+    if (depth==2):
+        for path in paths:
+            if not path.startswith('.'):
+                real_paths.extend([os.path.join(path, x) for x in os.listdir(os.path.join(input_path, path))])
+    else: real_paths = paths
+    random.shuffle(real_paths)
+    for filename, i in zip(real_paths, range(max_num)):
         img_path = os.path.join(input_path, filename)
         img = cv2.imread(img_path)
         height, width = img.shape[:2]
@@ -38,21 +47,23 @@ def generate_dataset(input_path, max_num, img_size):
         gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         gray_img = cv2.merge([gray_img, gray_img, gray_img])
 
-        img = cv2.resize(gray_img, (w, h))
+        gray_img = cv2.resize(gray_img, (w, h))
+        img = cv2.resize(img, (w, h))
+        filename = os.path.basename(filename)
         filename = os.path.splitext(filename)[0]
-        rgb_image = np.transpose(cv2.resize(img, (img_size, img_size)), (2, 0, 1))
-        output_path = os.path.join("rgb", f"processed_{filename}")
+        rgb_image = np.transpose(cv2.resize(gray_img, (img_size, img_size)), (2, 0, 1))
+        output_path = os.path.join(path_base + "rgb", f"processed_{filename}")
         np.save(output_path, np.array(rgb_image, dtype='uint8'))
 
         start_x = max(w // 2 - img_size // 2, 0)
         start_y = max(h // 2 - img_size // 2, 0)
         img = img[start_y:start_y + img_size, start_x:start_x + img_size]
-        output_path = os.path.join("color", f"processed_{filename}.npy")
+        output_path = os.path.join(path_base + "color", f"processed_{filename}.npy")
         yuv_image = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
         Y = yuv_image[:, :, 0]
         U = yuv_image[:, :, 1]
         V = yuv_image[:, :, 2]
-        # np.save(output_path, np.stack((U, V)))
-        output_path = os.path.join("gray", f"processed_{filename}")
-        # np.save(output_path, Y.reshape(1, img_size, img_size))
+        np.save(output_path, np.stack((U, V)))
+        output_path = os.path.join(path_base + "gray", f"processed_{filename}")
+        np.save(output_path, Y.reshape(1, img_size, img_size))
 
